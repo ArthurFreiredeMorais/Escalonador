ce80bda64f0c03367495460ad4115bbcdc3d80e0
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-04-17 20:49:56 -0300

    Update CpuManager.java

--- MM2/src/so/cpu/CpuManager.java
+++ MM2/src/so/cpu/CpuManager.java
@@ -9,29 +9,64 @@ import java.util.TimerTask;
 import so.SubProcess;
 
 public class CpuManager {
+    private Core[] cores;
+    public static int CLOCK = 500;
+    public static int NUMBER_OF_INSTRUCTIONS_BY_CLOCK = 7;
+    public static int NUMBER_OF_CORES = 4;
+    private Scheduler scheduler;
 
-	private Core[] cores;
-	public static int CLOCK = 5000;
-	
-	public static int 
-		NUMBER_OF_CORES = 4, 
-		NUMBER_OF_INSTRUCTIONS_PER_PROCESS = 7;
-	
-	
-	public CpuManager() {
-		this.cores = new Core[NUMBER_OF_CORES];
-		for (int i = 0; i < NUMBER_OF_CORES; i++) {
-			this.cores[i] = new Core(NUMBER_OF_INSTRUCTIONS_PER_PROCESS);
-		
-		}
-		this.clock();
-		
-		}
-		
-	public void registerProcess(int coreIndex, SubProcess sp) {
-		this.cores[coreIndex].setActuallySubProcess(sp);
-			
-		}
+    public CpuManager(Scheduler scheduler) {
+        this.cores = new Core[CpuManager.NUMBER_OF_CORES];
+
+        for (int index = 0; index < this.cores.length; index++) {
+            this.cores[index] = new Core(index, CpuManager.NUMBER_OF_INSTRUCTIONS_BY_CLOCK);
+        }
+        this.scheduler = scheduler;
+        this.runClock();
+    }
+
+    private void runClock() {
+        new Thread(() -> {
+            while (true) {
+                executeCores();
+                try {
+                    Thread.sleep(CpuManager.CLOCK);
+                } catch (InterruptedException e) {
+                	e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void executeCores() {
+    	int count = 0;
+        SubProcess data = null;
+        for (Core core : cores) {
+            data = this.scheduler.execute();
+            if (data != null) {
+                if (core.getSubProcess() == null) {
+                    core.setSubProcess(data);
+                    core.run();
+                    count++;
+                    if (this.scheduler != null && count == 4) {
+                    	if (this.scheduler.getQuantumTable() != null) {
+                    		
+                    		if (this.scheduler.getQuantumTable().get(data.getProcess().getId()) != 0) {        			
+                    			System.out.println(this.scheduler.getQuantumTable().get(data.getProcess().getId()));
+                    		}
+                    	}
+                	}
+                }
+            }
+        }
+
+        if (data != null) {
+        	System.out.println(" ");
+            System.out.println("##############################################");
+            System.out.println(" ");
+        }
+    }
+}
 	
 	public void clock () {
 		new Timer().scheduleAtFixedRate(new TimerTask() {
