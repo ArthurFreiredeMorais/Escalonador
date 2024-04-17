e885a782b15b9a0abd9dbea92e084fc011d1c4c8
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-04-17 20:47:57 -0300

    Update Core.java

--- MM2/src/so/cpu/Core.java
+++ MM2/src/so/cpu/Core.java
@@ -2,52 +2,44 @@ package so.cpu;
 
 import so.SubProcess;
 
-public class Core implements Runnable {
+public class Core {
 	private int id;
-	private int numberOfInstructionPerClock;
-	private SubProcess actuallySubProcess;
-	private int numberOfInstructionsExecuted;
-	
-	public Core (int numberOfInstructionPerClock, int id) {
-		this.numberOfInstructionPerClock = numberOfInstructionPerClock;
-		
-	}
-	
-	public Core (int id) {
-		this(id, 7);
+	private int numberOfInstructionsByClock;
+	private SubProcess subProcess;
+
+	public Core(int id, int numberOfInstructionsByClock) {
+		this.id = id;
+		this.numberOfInstructionsByClock = numberOfInstructionsByClock;
+		this.subProcess = null;
 	}
 
-	@Override
 	public void run() {
-		this.numberOfInstructionsExecuted += numberOfInstructionPerClock;
-		if (this.numberOfInstructionsExecuted >= actuallySubProcess.getInstructions()) {
-			this.finishSubProcess();		}
-		
+		if (this.subProcess != null) {
+			System.out.println(" ");
+			System.out.print("CPU - CORE " + this.id + " : ");
+			System.out.println(" " + this.subProcess.getId());
+			finish();
+		}
 	}
 
-	private void finishSubProcess() {
-		this.actuallySubProcess = null;
-		this.numberOfInstructionsExecuted = 0;
-		
-	}
-
-	public SubProcess getActuallySubProcess() {
-		return actuallySubProcess;
-	}
-
-	public void setActuallySubProcess(SubProcess actuallySubProcess) {
-		this.actuallySubProcess = actuallySubProcess;
+	private void finish() {
+		if (this.subProcess != null && this.subProcess.getProcess().getInstructions() > this.subProcess.getProcess().getInstructionsExecuted()) {
+			this.subProcess.finish();
+			this.subProcess.getProcess().setInstructionsExecuted(this.subProcess.getInstructions());
+			this.subProcess.getProcess().checkIfSubProcessIsDone();
+			this.subProcess = null;
+		}
 	}
 
 	public int getId() {
-		return id;
+		return this.id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public SubProcess getSubProcess() {
+		return this.subProcess;
 	}
-	
-	
-	
 
+	public void setSubProcess(SubProcess subProcess) {
+		this.subProcess = subProcess;
+	}
 }
